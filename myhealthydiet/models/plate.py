7cbdbdd521bed2e227019b7742c684cfd6b20583
Haizea <haizea.franco@gmail.com>
2022-12-16 13:49:25 +0100

    Plate model updated and demoPlate created

--- myhealthydiet/models/plate.py
+++ myhealthydiet/models/plate.py
@@ -7,7 +7,13 @@ class plate(models.Model):
     
     plate_id = fields.Integer(required=True, help="Id of the plate")
     plateName = fields.Char(required=True, help="Name of the plate")
-    mealTypeEnum = fields.Selection([("BREAKFAST", "BREAKFAST"), ("LUNCH", "LUNCH"), ("DINNER", "DINNER")], required=True, 
+    mealType = fields.Selection([("BREAKFAST", "BREAKFAST"), ("LUNCH", "LUNCH"), ("DINNER", "DINNER")], required=True, 
     help="Type of meal: Breakfast, Lunch, Dinner")
+    calories = fields.Float(required=True, help="Calories of the plate")
+    carbohydrates = fields.Float(required=True, help="Carbohydrates of the plate")
+    lipids = fields.Float(required=True, help="Lipids of the plate")
+    proteins = fields.Float(required=True, help="Proteins of the plate")
+    isVegetarian = fields.Boolean(required=True, help="Defines if the plate is vegetarian or not")
+    
     ingredients = fields.Many2many('myhealthydiet.ingredient')
     diets = fields.Many2many('myhealthydiet.diet')
